4a8d18861f6f21f8658999b2520865c08861f47a
MichaelR13 <michaels.rojas13@csu.fullerton.edu>
2023-02-02 17:42:12 -0800

    Updated header file

--- function.h
+++ function.h
@@ -1,10 +1,24 @@
-void readNames(){
-string myText;
-ifstream myFile("README.md");
+// Header file that reads and outputs the group members from README.md
 
-while (getline (myFile, myText)) {
-    // Output the text from the file
-    cout << myText;
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+void readNames(string filename){
+    string line;
+    ifstream myfile ("README.md");
+    if (myfile.is_open()){
+        while ( getline (myfile,line) ){
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+    else cout << "Unable to open file";
 }
-    myFile.close();
-}
+
+#endif
